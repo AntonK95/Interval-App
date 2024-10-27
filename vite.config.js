269b2d5bf2b5bca35fd819dcc7f63c72cb3fa2d3
Anton <148202281+AntonK95@users.noreply.github.com>
2024-10-27 14:24:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Interval-App' // Viktigt för GH-pages
+  base: '/Interval-App/' // Viktigt för GH-pages.
 })
